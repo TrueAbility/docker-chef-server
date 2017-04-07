23cfd6efdda340e53a23352b90fe9bb5ec359265
BJ Dierkes <derks@datafolklabs.com>
2017-04-07 12:43:52 -0500

    Fix Logic in Wait Lock Script

--- src/bin/wait-lock.sh
+++ src/bin/wait-lock.sh
@@ -2,16 +2,17 @@
 
 LOCK="/var/opt/.run/startup.lock"
 
-echo -n "Waiting for Chef Server Startup Lock"
+echo -n "Waiting for Chef Server Startup Lock..."
 while true; do
-    if [ ! -f $LOCK ]; then
+    if [ -f $LOCK ]; then
         echo
         echo "done"
         echo
         break
+    else
+        sleep 10
+        echo -n "."
     fi
-    sleep 10
-    echo -n "."
 done
 
 exit 0
